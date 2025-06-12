40a52a575f4197b0d3d4bddc4bcd21ecbcb425b2
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-06-13 10:29:49 +1200

    Update index.py

--- index.py
+++ index.py
@@ -12,4 +12,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request}) 
