0c53bc147fd8d495ade8c06062a1a1d4ae602670
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-06-22 15:00:51 +1200

    Update menu.js

--- menu.js
+++ menu.js
@@ -38,7 +38,6 @@ const configuracoesAnalises = {
   ],
   "Análise de controle de processo": [
     { nome: "Carta I-MR": },
-
     { nome: "Carta X-BarraR" },
     { nome: "Carta X-BarraS" },
     { nome: "Carta P" },
