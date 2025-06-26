98c10b72b7c42bdabef4431fadb06b7676b568fe
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-06-27 11:44:12 +1200

    Update entradadedados.js

--- entradadedados.js
+++ entradadedados.js
@@ -56,7 +56,7 @@ const configuracoesFerramentas = {
   // Análise Controle de Processo
   "Carta I-MR": ["Y"],
   "Carta X-BarraR": ["Y", "Subgrupo"],
-  "Carta X-BarraS": ["Y", "Subgrupo"],
+  "Carta X-BarraS": ["Y", "Subgrupo"], 
   "Carta P": ["Y", "Subgrupo"],
   "Carta NP": ["Y", "Subgrupo"],
   "Carta C": ["Y"],
