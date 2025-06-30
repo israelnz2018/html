054f6249105ce39c9a2a9b3dba25c9aca5914fc6
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-06-30 14:08:03 +1200

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi import FastAPI, Form
-from firebase_utils import gerar_link_reset_e_enviar
+
 
 app = FastAPI()
 
@@ -15,14 +14,4 @@ async def home():
 
 
 
-@app.post("/reset-senha")
-async def reset_senha(email: str = Form(...)):
-    """
-    Endpoint para gerar link de reset de senha
-    e enviar email bonito para o usuário.
-    """
-    sucesso = gerar_link_reset_e_enviar(email)
-    if sucesso:
-        return {"mensagem": "📧 Email de reset enviado com sucesso."}
-    else:
-        return {"mensagem": "❌ Falha ao enviar email de reset."}
+
