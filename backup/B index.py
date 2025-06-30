880e71940a17ad630f344af35a68a0566c6b08ff
israelnz2018 <105463483+israelnz2018@users.noreply.github.com>
2025-07-01 09:10:29 +1200

    Update and rename index.py to B index.py

--- backup/B index.py
+++ backup/B index.py
@@ -1,23 +1,29 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 
 app = FastAPI()
 
 # Serve arquivos estáticos da raiz do projeto
 app.mount("/", StaticFiles(directory=".", html=True), name="static")
 
+@app.get("/")
+async def home():
+    return FileResponse("index.html")
+
+# Middleware de CORS atualizado para incluir ambos os domínios
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://educacaopelotrabalho-production.up.railway.app",
-        "https://app.educacaopelotrabalho.com"
+        "https://app.educacaopelotrabalho.com",
+        "https://educacaopelotrabalho-production.up.railway.app"
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.get("/")
-async def home():
-    return FileResponse("index.html")
